416f772214ac92e80e61b6498f8e82c27ac5988e
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2023-12-01 12:32:03 -0300

    feat: insert UserEntity

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { hash } from 'bcrypt';
+
+import { UserEntity } from './interfaces/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRound = 10;
+
+    const passwordHashed = await hash(createUserDto.password, saltOrRound);
+
+    return this.userRepository.save({
+      ...createUserDto,
+      passwordHashed: passwordHashed,
+    });
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+}
